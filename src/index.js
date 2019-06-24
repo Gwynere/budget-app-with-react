e99ee4a68ea28226f38185420b3143c7582246ad
Gwynere <janpardak@gmail.com>
2019-06-24 18:52:21 +0200

    added most of the features, remains date, amount start and end date

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import AppRouter from './routers/AppRouter';
 import * as serviceWorker from './serviceWorker';
 import 'normalizecss/normalize.css';
 
+import uuid from 'uuid'
+
 ReactDOM.render(<AppRouter />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
@@ -16,9 +18,40 @@ serviceWorker.unregister();
 
 
 // ADD_EXPENSE
+const addExpense = (
+    {
+        description = '',
+        note = '',
+        amount = 0,
+        createdAt = 0
+    }) => ({
+    type: 'ADD_EXPENSE',
+    expense: {
+        id: uuid(),
+        description,
+        note,
+        amount,
+        createdAt
+    }
+})
+
 // REMOVE_EXPENSE
+const removeExpense = ({ id } = {}) => ({
+    type: 'REMOVE_EXPENSE',
+    id
+})
+
 // EDIT_EXPENSE
+const editExpense = (id, updates) => ({
+    type: 'EDIT_EXPENSE',
+    id
+})
+
 // SET TEXT FILTER
+const setTextFilter = ( text = '' ) => ({
+    type: 'SET_TEXT_FILTER',
+    text
+})
 // SORT BY DATE
 // SORT BY AMOUNT
 // SET START DATE
@@ -31,6 +64,34 @@ const expensesReducerDefaultState = []
 
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
     switch (action.type) {
+        case 'ADD_EXPENSE':
+            return [
+                ...state,
+                action.expense
+            ]
+
+        case 'REMOVE_EXPENSE':
+            return state.filter(({ id }) => {
+                return id !== action.id
+            })
+
+        case 'EDIT_EXPENSE':
+            return state.map((expense) => {
+                if(expense.id === action.id) {
+                    return {
+                        ...expense,
+                        ...action.updates
+                    }
+                } else{
+                    return expense
+                }
+            })
+
+        case 'SET_TEXT_FILTER':
+            return {
+                ...state,
+                text: action.text
+            }
         default:
             return state
     }
@@ -59,7 +120,18 @@ const store = createStore(
     })
 )
 
-console.log(store.getState())
+store.subscribe(() => {
+    console.log(store.getState())
+})
+
+const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100 }))
+const expenseTwo = store.dispatch(addExpense({ description: 'Rent', amount: 100 }))
+
+store.dispatch(removeExpense({ id: expenseOne.expense.id }))
+
+store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500}))
+
+store.dispatch(setTextFilter('rent'))
 
 const demoState = {
     expenses: [{
@@ -76,3 +148,14 @@ const demoState = {
         endDate: undefined
     }
 };
+
+const user = {
+    name: 'Jen',
+    age: 24
+}
+
+console.log({
+    age: 44,
+    ...user,
+    location: 'Philadelphia'
+})
