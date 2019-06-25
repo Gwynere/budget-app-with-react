c2afa4334cf83cd8a7f56f29b061689731b9f569
Gwynere <janpardak@gmail.com>
2019-06-25 19:18:49 +0200

    react with redux section finished, the application is working correctly, time for testing with jest

--- src/index.js
+++ src/index.js
@@ -1,27 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import configureStore from './store/configureStore'
+import { Provider } from "react-redux";
+import configureStore from "./store/configureStore";
 import AppRouter from "./routers/AppRouter";
 import * as serviceWorker from "./serviceWorker";
 import "normalizecss/normalize.css";
 
-import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
+import { addExpense } from "./actions/expenses";
+import getVisibleExpenses from "./selectors/expenses";
 
+const store = configureStore();
 
-const store = configureStore()
+store.dispatch(addExpense({ description: "Water bill" , amount: 4500}));
+store.dispatch(addExpense({ description: "Gas bill", createdAt: 1000 }));
+store.dispatch(addExpense({ description: "Rent", amount: 109500 }));
+store.dispatch(addExpense({ description: 'Car Insurance', amount: 10300}))
+// store.dispatch(setTextFilter("water"));
 
-store.dispatch(addExpense({ description: 'Water bill'}))
-store.dispatch(addExpense({ description: 'Gas bill'}))
-store.dispatch(setTextFilter('water'))
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 
-const state = store.getState()
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
+console.log(visibleExpenses);
 
-console.log(visibleExpenses)
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
 
-ReactDOM.render(<AppRouter />, document.getElementById("root"));
+ReactDOM.render(jsx, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
